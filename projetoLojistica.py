c9f39b9251ce6a4ab79e3dbebc99880a8bceaa83
Lucas Ávila <avilalucas11@gmail.com>
2023-05-20 13:02:44 -0300

    Acrescentado comentários

--- projetoLojistica.py
+++ projetoLojistica.py
@@ -1,9 +1,11 @@
 def dimensoesObjeto():
     while True:
+        # Criei a função "Try" para caso acontece um erro de digitação, por parte do usuário
         try:
             comprimento = float(input("Digite o comprimento do objeto (em cm): "))
             largura = float(input("Digite a largura do objeto (em cm): "))
             altura = float(input("Digtie a altura do objeto (em cm): "))
+            # Cálculo total dos pedidos
             volume = comprimento * largura * altura
             print(f"O valor do objeto é (em cm³): {volume}")
             if volume < 1000:
@@ -17,7 +19,9 @@ def dimensoesObjeto():
             else:
                 print("Não aceitamos objetos com dimensões tão grandes")
                 print("Entre com as dimensões novamente")
+            # Caso o usuário digite um valor superior à 100000, não é aceito e pede novamente ás informações do pedido
                 continue
+            # Se o usuário digitar um caractere, imprime a imagem abaixo informando que foi informado uma dimensão não numérico
         except ValueError:
             print("Você digitou alguma dimensão do objeto não numérico")
             print("Por favor entre com as dimensões desejados novamente")
@@ -25,6 +29,7 @@ def dimensoesObjeto():
 
 def pesoObjeto():
     while True:
+        # Criei a função "Try" para caso acontece um erro de digitação, por parte do usuário
         try:
             peso = float(input("Digite o peso do objeto (em kg): "))
             if peso == 0.1:
@@ -38,7 +43,9 @@ def pesoObjeto():
             else:
                 print("Não aceitamos objetos tão pesados")
                 print("Entre com o peso desejado novamente")
+            # Caso o usuário digite um valor superior à 30, não é aceito e pede novamente ás informações do pedido
                 continue
+        # Se o usuário digitar um caractere, imprime a imagem abaixo informando que foi informado uma dimensão não numérico
         except ValueError:
             print("Você digitou peso do objeto com valor não numérico")
             print("Por favor entre com o peso desejado novamente")
@@ -66,9 +73,11 @@ def rotasObjeto():
             return 1.5
         elif rota == "RB":
             return 1.5
+        # Se o usuário digitar uma rota diferente das mencionadas, imprime ás mensagens abaixo
         else:
             print("Você digitou uma rota que não existe")
             print("Por favor entre com a rota desejada novamente")
+        # Função "continue" é para retornar ao "while True", e assim poder informar ás rotas novamente, de forma correta
             continue
 
 
@@ -76,5 +85,6 @@ print("Bem Vindo a Companhia de Logística Lucas Lima de Ávila")
 dimensoes = dimensoesObjeto()
 peso = pesoObjeto()
 rotas = rotasObjeto()
+# Criei a variável "total" para realizar à soma das dimensões, peso e rotas
 total = dimensoes * peso * rotas
 print(f"Total a pagar (R$): {total:.2f} (dimensões: {dimensoes} * peso: {peso} * rotas: {rotas})")
